fcdefd656d29a9bca782f8efabe2099522f47dfc
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2018-04-25 00:14:23 +0300

    исправлен запуск

--- java/ru/ifmo/rain/kokorin/crawler/BookDownloader.java
+++ java/ru/ifmo/rain/kokorin/crawler/BookDownloader.java
@@ -2,11 +2,8 @@ package ru.ifmo.rain.kokorin.crawler;
 
 import info.kgeorgiy.java.advanced.crawler.CachingDownloader;
 import info.kgeorgiy.java.advanced.crawler.Crawler;
-import info.kgeorgiy.java.advanced.crawler.Downloader;
-import info.kgeorgiy.java.advanced.crawler.Result;
 import info.kgeorgiy.java.advanced.crawler.URLUtils;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -16,7 +13,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Calendar;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class BookDownloader {
@@ -25,37 +21,34 @@ public class BookDownloader {
     private final static String BIBL_RECORD_BEGIN = "<div id=\"bibliographic_record\">";
     private final static String BIBL_RECORD_END = "</div>";
     private final static byte OK_MARKER = '+';
+    private final static int CODES[] = {917, 918, 1537};
+    private static final String HEADER_TEMPLATE = "https://e.lanbook.com/books/%d";
+    private static final String PAGE_TEMPLATE = "https://e.lanbook.com/books/%d?page=";
+    private static final String ERROR_MSG = "running:\n" +
+            "BookDownloader --all <folder for storing downloaded files> <file for storing result>\n" +
+            "BookDownloader --download <folder for storing downloaded files>\n" +
+            "BookDownloader --parse <folder for storing downloaded files> <file for storing result>";
 
     private static int MAX_YEAR = Calendar.getInstance().get(Calendar.YEAR);
     private static int MIN_YEAR = MAX_YEAR - 4;
 
     private static boolean containsCode(String url) {
-        return url.equals("https://e.lanbook.com/books/917") ||
-                url.equals("https://e.lanbook.com/books/918") ||
-                url.equals("https://e.lanbook.com/books/1537") ||
-                url.startsWith("https://e.lanbook.com/books/917?page=") ||
-                url.startsWith("https://e.lanbook.com/books/918?page=") ||
-                url.startsWith("https://e.lanbook.com/books/1537?page=");
+        for (int code : CODES) {
+            if (url.equals(String.format(HEADER_TEMPLATE, code)) ||
+                    url.startsWith(String.format(PAGE_TEMPLATE, code))) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public static void main(String[] args) {
-
-        if (args == null || args.length != 2 || args[0] == null || args[1] == null) {
-            System.out.println("running: BookDownloader <folder for storing downloaded files> <file for storing result>");
-            return;
-        }
-
-        Path dirForDownloadedFiles = Paths.get(args[0]);
-        Path pathToRes = Paths.get(args[1]);
-
+    private static void download(Path dirForDownloadedFiles) {
         try {
             Files.createDirectories(dirForDownloadedFiles);
-            Files.createDirectories(pathToRes.toAbsolutePath().getParent());
         } catch (IOException e) {
-            System.err.println("Cannot create directory " + e.getMessage());
+            System.err.println("Cannot create directory for downloaded files" + e.getMessage());
             return;
         }
-
         String host;
         try {
             host = URLUtils.getHost("https://e.lanbook.com/books");
@@ -86,7 +79,16 @@ public class BookDownloader {
         } catch (IOException e) {
             System.err.println("Error downloading " + e.getMessage());
         }
+    }
 
+    private static void parse(Path dirForDownloadedFiles, Path pathToRes) {
+        try {
+            Files.createDirectories(pathToRes.toAbsolutePath().getParent());
+            Files.createDirectories(dirForDownloadedFiles);
+        } catch (IOException e) {
+            System.err.println("Cannot create directory" + e.getMessage());
+            return;
+        }
         try (BufferedWriter writer = Files.newBufferedWriter(pathToRes)) {
             Files.list(dirForDownloadedFiles).forEach(
                     page -> {
@@ -95,7 +97,7 @@ public class BookDownloader {
                         }
                         try (InputStream is = Files.newInputStream(page)) {
                             int x = is.read();
-                            if (x == -1 || (byte)x != OK_MARKER) {
+                            if (x == -1 || (byte) x != OK_MARKER) {
                                 return;
                             }
                         } catch (IOException e) {
@@ -135,7 +137,50 @@ public class BookDownloader {
                     }
             );
         } catch (IOException e) {
-            System.err.println("I/O error occurred " + e.getMessage());
+            System.err.println("Error while writing to file " + e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+
+        if (args == null || args.length < 1 || args.length > 3 || args[0] == null) {
+            System.out.println(ERROR_MSG);
+            return;
+        }
+
+        if (args[0].equals("--all")) {
+            if (args.length != 3 || args[1] == null || args[2] == null) {
+                System.out.println(ERROR_MSG);
+                return;
+            }
+            Path dirForDownloadedFiles = Paths.get(args[1]);
+            Path pathToRes = Paths.get(args[2]);
+            download(dirForDownloadedFiles);
+            parse(dirForDownloadedFiles, pathToRes);
+            return;
+        }
+
+        if (args[0].equals("--download")) {
+            if (args.length != 2 || args[1] == null) {
+                System.out.println(ERROR_MSG);
+                return;
+            }
+            Path dirForDownloadedFiles = Paths.get(args[1]);
+            download(dirForDownloadedFiles);
+            return;
+        }
+
+        if (args[0].equals("--parse")) {
+            if (args.length != 3 || args[1] == null || args[2] == null) {
+                System.out.println(ERROR_MSG);
+                return;
+            }
+            Path dirForDownloadedFiles = Paths.get(args[1]);
+            Path pathToRes = Paths.get(args[2]);
+            parse(dirForDownloadedFiles, pathToRes);
+            return;
+        }
+
+        System.out.println(ERROR_MSG);
     }
 }
