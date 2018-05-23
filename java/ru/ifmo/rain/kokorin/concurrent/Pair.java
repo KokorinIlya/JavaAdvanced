b1ab7d71b15d08c69ab5afaf048b338edfe44281
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2018-05-23 23:26:32 +0300

    Добавлена пара

---
+++ java/ru/ifmo/rain/kokorin/concurrent/Pair.java
@@ -0,0 +1,43 @@
+package ru.ifmo.rain.kokorin.concurrent;
+
+public class Pair<K,V> {
+
+    private K key;
+
+    public K getKey() { return key; }
+
+    private V value;
+
+    public V getValue() { return value; }
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return key + "=" + value;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + (key != null ? key.hashCode() : 0);
+        hash = 31 * hash + (value != null ? value.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o instanceof Pair) {
+            Pair pair = (Pair) o;
+            if (key != null ? !key.equals(pair.key) : pair.key != null) return false;
+            if (value != null ? !value.equals(pair.value) : pair.value != null) return false;
+            return true;
+        }
+        return false;
+    }
+}
+
